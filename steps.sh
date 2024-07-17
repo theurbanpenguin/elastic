b2f1b077e6e9d58faf798d7ba66de8cee7f82ed4
TheUrbanPenguin <andrew.mallett@theurbanpenguin.com>
2024-07-17 19:12:50 +0100

    First tidy

--- steps.sh
+++ steps.sh
@@ -1,3 +1,5 @@
+## Elasticsearch
+
 sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 sudo tee /etc/yum.repos.d/elastic.repo <<EOF
 [elasticsearch]
@@ -6,11 +8,130 @@ baseurl=https://artifacts.elastic.co/packages/8.x/yum
 gpgcheck=1
 gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
 enabled=1
-autorefresh=1
 type=rpm-md
 EOF
-sudo yum install -y elasticsearch
 
+ sudo yum install -y elasticsearch
+sudo tee /etc/elasticsearch/elasticsearch.yml <<EOF
+# ======================== Elasticsearch Configuration =========================
+#
+# NOTE: Elasticsearch comes with reasonable defaults for most settings.
+#       Before you set out to tweak and tune the configuration, make sure you
+#       understand what are you trying to accomplish and the consequences.
+#
+# The primary way of configuring a node is via this file. This template lists
+# the most important settings you may want to configure for a production cluster.
+#
+# Please consult the documentation for further information on configuration options:
+# https://www.elastic.co/guide/en/elasticsearch/reference/index.html
+#
+# ---------------------------------- Cluster -----------------------------------
+#
+# Use a descriptive name for your cluster:
+#
+cluster.name: my-application
+#
+# ------------------------------------ Node ------------------------------------
+#
+# Use a descriptive name for the node:
+#
+node.name: node-1
+#
+# Add custom attributes to the node:
+#
+#node.attr.rack: r1
+#
+# ----------------------------------- Paths ------------------------------------
+#
+# Path to directory where to store the data (separate multiple locations by comma):
+#
+path.data: /var/lib/elasticsearch
+#
+# Path to log files:
+#
+path.logs: /var/log/elasticsearch
+#
+# ----------------------------------- Memory -----------------------------------
+#
+# Lock the memory on startup:
+#
+#bootstrap.memory_lock: true
+#
+# Make sure that the heap size is set to about half the memory available
+# on the system and that the owner of the process is allowed to use this
+# limit.
+#
+# Elasticsearch performs poorly when the system is swapping the memory.
+#
+# ---------------------------------- Network -----------------------------------
+#
+# By default Elasticsearch is only accessible on localhost. Set a different
+# address here to expose this node on the network:
+#
+network.host: 0.0.0.0
+#
+# By default Elasticsearch listens for HTTP traffic on the first free port it
+# finds starting at 9200. Set a specific HTTP port here:
+#
+#http.port: 9200
+#
+# For more information, consult the network module documentation.
+#
+# --------------------------------- Discovery ----------------------------------
+#
+# Pass an initial list of hosts to perform discovery when this node is started:
+# The default list of hosts is ["127.0.0.1", "[::1]"]
+#
+# discovery.seed_hosts: ["127.0.0.1"]
+#
+# Bootstrap the cluster using an initial set of master-eligible nodes:
+#
+cluster.initial_master_nodes: ["node-1"]
+#
+# For more information, consult the discovery and cluster formation module documentation.
+#
+# ---------------------------------- Various -----------------------------------
+#
+# Allow wildcard deletion of indices:
+#
+#action.destructive_requires_name: false
+
+#----------------------- BEGIN SECURITY AUTO CONFIGURATION -----------------------
+#
+# The following settings, TLS certificates, and keys have been automatically
+# generated to configure Elasticsearch security features on 15-07-2024 10:32:21
+#
+# --------------------------------------------------------------------------------
+
+# Enable security features
+xpack.security.enabled: false
+
+xpack.security.enrollment.enabled: false
+
+# Enable encryption for HTTP API client connections, such as Kibana, Logstash, and Agents
+xpack.security.http.ssl:
+  enabled: false
+  keystore.path: certs/http.p12
+
+# Enable encryption and mutual authentication between cluster nodes
+xpack.security.transport.ssl:
+  enabled: false
+  verification_mode: certificate
+  keystore.path: certs/transport.p12
+  truststore.path: certs/transport.p12
+# Create a new cluster with the current node only
+# Additional nodes can still join the cluster later
+
+# Allow HTTP API connections from anywhere
+# Connections are encrypted and require user authentication
+http.host: 0.0.0.0
+
+# Allow other nodes to join the cluster from anywhere
+# Connections are encrypted and mutually authenticated
+#transport.host: 0.0.0.0
+
+#----------------------- END SECURITY AUTO CONFIGURATION -------------------------
+EOF
 
 sudo systemctl daemon-reload
 sudo systemctl enable --now elasticsearch
@@ -27,9 +148,23 @@ curl -X POST "localhost:9200/test-index/_doc/1?pretty" -H 'Content-Type: applica
 curl -X GET "localhost:9200/test-index/_doc/1?pretty"
 curl -X GET "localhost:9200/test-index/_search?q=name:John&pretty"
 
+## Logstash
+
+
+yum repolist
 sudo yum install -y logstash
-#sudo /usr/share/logstash/bin/logstash -f /etc/logstash/conf.d/stdin.conf
-#curl -X GET "localhost:9200/stdin-log-*/_search?pretty"
+tee ~/pipe1.conf <<EOF
+input {
+    stdin {}
+}
+
+output {
+    stdout { codec => rubydebug }
+}
+EOF
+
+sudo /usr/share/logstash/bin/logstash -f ~vagrant/pipe1.conf
+
 sudo vim /etc/logstash/conf.d/logstash-http.conf
 sudo systemctl enable --now logstash
 
@@ -37,3 +172,10 @@ python3 email.py
 curl -X GET "localhost:9200/python-emails-*/_search?pretty"
 curl -X GET "localhost:9200/python-emails-*/_search?q=email:outlook.com&pretty"
 curl -X GET "localhost:9200/python-emails-*/_count?pretty"
+curl -X GET "localhost:9200/dnf_logs*/_search?pretty"
+curl -X GET "localhost:9200/dnf_logs*/_search?pretty" -H 'Content-Type: application/json' -d'
+{
+  "size": 33,
+  "_source": ["message"]
+}
+'
